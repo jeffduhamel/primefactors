32492a91cd543992689dbf91c3de170fd4ecd5c4
Duhamel, Jeffrey <jeffduhamel@yahoo.com>
2020-12-20 19:11:34 -0600

    Add test for rest controller and error page

--- PrimeFactors/src/main/java/com/duhamel/primefactors/controller/PrimeFactorsRestController.java
+++ PrimeFactors/src/main/java/com/duhamel/primefactors/controller/PrimeFactorsRestController.java
@@ -1,5 +1,24 @@
 package com.duhamel.primefactors.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.duhamel.primefactors.services.PrimeFactorsService;
+
+@RestController
 public class PrimeFactorsRestController {
+	
+	@Autowired
+	private PrimeFactorsService primeFactorsService;
+	
+	@RequestMapping(method = RequestMethod.GET, path="/factors", produces = "application/json")
+	ResponseEntity<?> getPrimeFactors(@RequestParam int number) {
+		return new ResponseEntity<>(primeFactorsService.factor(number), HttpStatus.OK);
+	}
 
 }
--- PrimeFactors/src/test/java/com/duhamel/primefactors/controllers/PrimeFactorsRestControllerTest.java
+++ PrimeFactors/src/test/java/com/duhamel/primefactors/controllers/PrimeFactorsRestControllerTest.java
@@ -1,15 +1,22 @@
 package com.duhamel.primefactors.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.*;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-
 import com.duhamel.primefactors.services.PrimeFactorsService;
 
 @WebMvcTest
@@ -23,8 +30,15 @@ class PrimeFactorsRestControllerTest {
 	PrimeFactorsService primeFactorsService;
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void validRequestProducesListOfValues() throws Exception {
+		ArrayList factors = new ArrayList();
+		factors.add(7);
+		factors.add(7);
+		factors.add(5);
+		when(primeFactorsService.factor(245)).thenReturn(factors);
+        mockMvc.perform(MockMvcRequestBuilders.get("/factors?number=245")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(jsonPath("$", containsInAnyOrder(5, 7, 7))).andExpect(status().isOk()).andDo(print());
 	}
 
 }
